191cc143d3578a92d738e9e63032dd563221ac01
duckling2211 <lmduc2211@gmail.com>
2025-10-02 07:18:07 +0700

    Delete Player.h

--- Player.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "GameObject.h"
-
-class Player : public GameObject {
-public:
-    char getSymbol() const override { return '@'; }
-    bool isWalkable() const override { return false; }
-    void interact(GameObject*) override {}
-};
