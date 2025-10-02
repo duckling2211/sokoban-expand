a15397c12fc628f9c31d594ff7d6455819fa2165
duckling2211 <lmduc2211@gmail.com>
2025-10-02 07:15:32 +0700

    Delete Breakable.h

--- Breakable.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "GameObject.h"
-
-class Breakable : public GameObject {
-public:
-    char getSymbol() const override { return 'B'; }
-    bool isWalkable() const override { return false; }
-    void interact(GameObject*) override {}
-};
