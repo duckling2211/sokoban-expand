adec7239e1a0050eb3523f8becc1ac1569f7225e
duckling2211 <lmduc2211@gmail.com>
2025-10-02 07:15:59 +0700

    Delete FloorTile.h

--- FloorTile.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "GameObject.h"
-
-class Floor : public GameObject {
-public:
-    char getSymbol() const override { return ' '; }
-    bool isWalkable() const override { return true; }
-    void interact(GameObject*) override {}
-};
