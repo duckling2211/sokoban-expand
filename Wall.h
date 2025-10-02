d29de1a8daf3ae018191496f999b04505ebf656f
duckling2211 <lmduc2211@gmail.com>
2025-10-02 07:18:23 +0700

    Delete Wall.h

--- Wall.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "GameObject.h"
-
-class Wall : public GameObject {
-public:
-    char getSymbol() const override { return '#'; }
-    bool isWalkable() const override { return false; }
-    void interact(GameObject*) override {}
-};
