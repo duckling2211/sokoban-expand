13f2c72b184cfdd671e1c831d56a8054f414dbc7
duckling2211 <lmduc2211@gmail.com>
2025-10-02 07:17:20 +0700

    Delete GoalTile.h

--- GoalTile.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "GameObject.h"
-
-class Goal : public GameObject {
-public:
-    char getSymbol() const override { return '.'; }
-    bool isWalkable() const override { return true; }
-    void interact(GameObject*) override {}
-};
